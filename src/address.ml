9822555ae82ba298e3214babe0ec0ca5b7f7f72f
Aaron Dufour <thedufer@gmail.com>
2018-01-16 23:22:40 -0500

    change address to int63, and add some helpers

--- src/address.ml
+++ src/address.ml
@@ -1,3 +1,9 @@
 open Core
 
-include Unique_id.Int ()
+include Unique_id.Int63 ()
+
+(* These can't raise when [int] is [int63], which I expect to be the case. *)
+let of_int = of_int_exn
+let to_int = to_int_exn
+
+let arg_type = Command.Arg_type.map (Command.Param.int) ~f:of_int
--- src/address.mli
+++ src/address.mli
@@ -5,3 +5,8 @@ type t [@@deriving bin_io, compare, sexp]
 include Comparable with type t := t
 
 val create : unit -> t
+
+val of_int : int -> t
+val to_int : t -> int
+
+val arg_type : t Command.Arg_type.t
